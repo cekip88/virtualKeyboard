31c4ccc69e200a296753b0ce587fa878aa017b30
cekip88 <cekip@mail.ru>
2020-10-29 19:57:17 +0600

    Надо сделать переключение языка, обновление символов на кнопках и правильную работу клавиш в паре с ctrl и bsp

--- main.js
+++ main.js
@@ -1,84 +1,131 @@
-class Keyboard {
-
-    constructor() {
-        const _ = this;
+class Keyboard {constructor() {const _ = this;
         _.body = document.querySelector('.keyboard');
-        _.buttons = [
-            {keyCode: 192, code: "Backquote", key: "`"},
-            {keyCode: 49, code: "Digit1", key: "1"},
-            {keyCode: 50, code: "Digit2", key: "2"},
-            {keyCode: 51, code: "Digit3", key: "3"},
-            {keyCode: 52, code: "Digit4", key: "4"},
-            {keyCode: 53, code: "Digit5", key: "5"},
-            {keyCode: 54, code: "Digit6", key: "6"},
-            {keyCode: 55, code: "Digit7", key: "7"},
-            {keyCode: 56, code: "Digit8", key: "8"},
-            {keyCode: 57, code: "Digit9", key: "9"},
-            {keyCode: 48, code: "Digit0", key: "0"},
-            {keyCode: 189, code: "Minus", key: "-"},
-            {keyCode: 187, code: "Equal", key: "="},
-            {keyCode: 81, code: "KeyQ", key: "q"},
-            {keyCode: 87, code: "KeyW", key: "w"},
-            {keyCode: 69, code: "KeyE", key: "e"},
-            {keyCode: 82, code: "KeyR", key: "r"},
-            {keyCode: 84, code: "KeyT", key: "t"},
-            {keyCode: 89, code: "KeyY", key: "y"},
-            {keyCode: 85, code: "KeyU", key: "u"},
-            {keyCode: 73, code: "KeyI", key: "i"},
-            {keyCode: 79, code: "KeyO", key: "o"},
-            {keyCode: 80, code: "KeyP", key: "p"},
-            {keyCode: 219, code: "BracketLeft", key: "["},
-            {keyCode: 221, code: "BracketRight", key: "]"},
-            {keyCode: 8, code: "Backspace", key: "Bsp"},
-            {keyCode: 20, code: "CapsLock", key: "Caps"},
-            {keyCode: 65, code: "KeyA", key: "a"},
-            {keyCode: 83, code: "KeyS", key: "s"},
-            {keyCode: 68, code: "KeyD", key: "d"},
-            {keyCode: 70, code: "KeyF", key: "f"},
-            {keyCode: 71, code: "KeyG", key: "g"},
-            {keyCode: 72, code: "KeyH", key: "h"},
-            {keyCode: 74, code: "KeyJ", key: "j"},
-            {keyCode: 75, code: "KeyK", key: "k"},
-            {keyCode: 76, code: "KeyL", key: "l"},
-            {keyCode: 186, code: "Semicolon", key: ";"},
-            {keyCode: 222, code: "Quote", key: "'"},
-            {keyCode: 13, code: "Enter", key: "Enter"},
-            {keyCode: 16, code: "ShiftLeft", key: "Shift"},
-            {keyCode: 90, code: "KeyZ", key: "z"},
-            {keyCode: 88, code: "KeyX", key: "x"},
-            {keyCode: 67, code: "KeyC", key: "c"},
-            {keyCode: 86, code: "KeyV", key: "v"},
-            {keyCode: 66, code: "KeyB", key: "b"},
-            {keyCode: 78, code: "KeyN", key: "n"},
-            {keyCode: 77, code: "KeyM", key: "m"},
-            {keyCode: 188, code: "Comma", key: ","},
-            {keyCode: 190, code: "Period", key: "."},
-            {keyCode: 191, code: "Slash", key: "/"},
-            {keyCode: 32, code: "Space", key: " "}
-        ];
+        _.area = document.querySelector('#input');
+        _.lang = 'en';
+        _.shift = false;
+        _.caps = false;
+        _.buttons = {'Backquote': {key: "`", 'shift': '~'},
+            'Digit1': {key: "1", 'shift': '!'},
+            'Digit2': {key: "2", 'shift': '@', 'ruShift': '"'},
+            'Digit3': {key: "3", 'shift': '#', 'ruShift': '№'},
+            'Digit4': {key: "4", 'shift': '$', 'ruShift': ';'},
+            'Digit5': {key: "5", 'shift': '%'},
+            'Digit6': {key: "6", 'shift': '^', 'ruShift': ':'},
+            'Digit7': {key: "7", 'shift': '&', 'ruShift': '?'},
+            'Digit8': {key: "8", 'shift': '*'},
+            'Digit9': {key: "9", 'shift': '('},
+            'Digit0': {key: "0", 'shift': ')'},
+            'Minus': {key: "-", 'shift': '_'},
+            'Equal': {key: "=", 'shift': '+'},
+            'KeyQ': {key: "q", ruKey: 'й'},
+            'KeyW': {key: "w", ruKey: 'ц'},
+            'KeyE': {key: "e", ruKey: 'у'},
+            'KeyR': {key: "r", ruKey: 'к'},
+            'KeyT': {key: "t", ruKey: 'е'},
+            'KeyY': {key: "y", ruKey: 'н'},
+            'KeyU': {key: "u", ruKey: 'г'},
+            'KeyI': {key: "i", ruKey: 'ш'},
+            'KeyO': {key: "o", ruKey: 'щ'},
+            'KeyP': {key: "p", ruKey: 'з'},
+            'BracketLeft': {key: "[", 'shift': ': {', ruKey: 'х'},
+            'BracketRight': {key: "]", 'shift': '}', ruKey: 'ъ'},
+            'Backspace': {key: "Bsp"},
+            'CapsLock': {key: "Caps"},
+            'KeyA': {key: "a", ruKey: 'ф'},
+            'KeyS': {key: "s", ruKey: 'ы'},
+            'KeyD': {key: "d", ruKey: 'в'},
+            'KeyF': {key: "f", ruKey: 'а'},
+            'KeyG': {key: "g", ruKey: 'п'},
+            'KeyH': {key: "h", ruKey: 'р'},
+            'KeyJ': {key: "j", ruKey: 'о'},
+            'KeyK': {key: "k", ruKey: 'л'},
+            'KeyL': {key: "l", ruKey: 'д'},
+            'Semicolon': {key: ";", 'shift': ':', ruKey: 'ж'},
+            'Quote': {key: "'", 'shift': '"', ruKey: 'э'},
+            'Enter': {key: "Enter"},
+            'ShiftLeft': {key: "Shift"},
+            'KeyZ': {key: "z", ruKey: 'я'},
+            'KeyX': {key: "x", ruKey: 'ч'},
+            'KeyC': {key: "c", ruKey: 'с'},
+            'KeyV': {key: "v", ruKey: 'м'},
+            'KeyB': {key: "b", ruKey: 'и'},
+            'KeyN': {key: "n", ruKey: 'т'},
+            'KeyM': {key: "m", ruKey: 'ь'},
+            'Comma': {key: ",", 'shift': '<', ruKey: 'б'},
+            'Period': {key: ".", 'shift': '>', ruKey: 'ю'},
+            'Slash': {key: "/", 'shift': '?', ruKey: '.', 'ruShift': ','},
+            'Space': {key: " "}
+    };
         _.init();
     }
 
+    k_key_light(code){
+        let btn = document.querySelector(`.${code}`);
+        if (btn && !btn.classList.contains('ShiftLeft') && !btn.classList.contains('CapsLock')){
+            if (!btn.classList.contains('active')) btn.classList.add('active');
+            setTimeout(function () {
+                btn.classList.remove('active')
+            },250)
+        }
+    }
+    buttonAction(code){
+        const _ = this;
+        let button = _.buttons[code];
+        if (!button) return;
+        let key = button.key;
+
+        if (key === 'Bsp'){
+            _.area.value = _.area.value.substring(0,_.area.value.length - 1);
+        } else if (key === 'Shift'){
+            _.shift = !_.shift;
+            document.querySelector(`.${code}`).classList.toggle('active');
+        } else if (key === 'Caps'){
+            _.caps = !_.caps;
+            document.querySelector(`.${code}`).classList.toggle('active');
+        } else {
+            let symbol = (_.shift && button.shift) ? button.shift : key;
+            symbol = (_.shift || _.caps) ? symbol.toUpperCase() : symbol;
+            _.area.value += symbol;
+        }
+
+        _.k_key_light(code);
+    }
+    keyBoardHandlers(){
+        const _ = this;
+        document.querySelectorAll('.keyboard button').forEach(function (button) {
+            button.addEventListener('click',function (e) {
+                let code = button.className;
+                code = code.split(' ')[0];
+                _.buttonAction(code);
+            });
+        });
+        _.area.addEventListener('keydown',function (e) {
+            if (_.buttons[e.code]) e.preventDefault();
+            _.buttonAction(e.code);
+        });
+    }
+
+
+
     createButtons(){
         const _ = this;
-        for (let i = 0; i < _.buttons.length; i++){
+        let i = 0;
+        for (let code in _.buttons){
             let button = document.createElement('BUTTON');
-            button.setAttribute('data-keyCode',_.buttons[i].keyCode);
-            button.setAttribute('data-code',_.buttons[i].code);
-            button.textContent = _.buttons[i].key;
+            button.setAttribute('class', code);
+            button.textContent = _.buttons[code]['key'];
             let rows = document.querySelectorAll('.row');
             if(i < 13) rows[0].append(button);
             if(i >= 13 && i < 26) rows[1].append(button);
             if(i >= 26 && i < 39) rows[2].append(button);
             if(i >= 39 && i < 50) rows[3].append(button);
             if(i >= 50) rows[4].append(button);
+            i++;
         }
     }
-
     createKeyboardTemp(){
         const _ = this;
-        for (let i = 0; i < 5; i++){
-            let row = document.createElement('DIV');
+        for (let i = 0; i < 5; i++){let row = document.createElement('DIV');
             row.className = 'row';
             _.body.append(row);
         }
@@ -88,11 +135,8 @@ class Keyboard {
         const _ = this;
         _.createKeyboardTemp();
         _.createButtons();
+        _.keyBoardHandlers();
     }
 }
 
 let keyboard = new Keyboard();
-
-document.addEventListener('keydown',function (e) {
-    console.log(e);
-})
